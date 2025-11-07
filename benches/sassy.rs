a180550d2c451a441ad1afd1ab48106d6e3f47f3
rickbeeloo <r.beeloo@outlook.com>
2025-11-07 12:58:35 +0100

    bump version

--- benches/sassy.rs
+++ benches/sassy.rs
@@ -1,4 +1,4 @@
-use mini_myers::{Positions, Scan, Searcher as mini_searcher, TQueries, U32};
+use mini_myers::{Positions, Scan, Searcher as mini_searcher, TQueries, U32, U64};
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 use sassy::profiles::Iupac;
@@ -41,11 +41,11 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     let mut rng = StdRng::seed_from_u64(42);
     let mut results = Vec::new();
 
-    let target_lens = vec![1_000, 10_000, 50_000, 1_000_000];
-    let query_lens = vec![24];
-    let ks = vec![4];
+    let target_lens = vec![1_000, 10_000, 50_000, 100_000, 1_000_000];
+    let query_lens = vec![64];
+    let ks = vec![14];
     let iterations = 100;
-    let n_queries = 192;
+    let n_queries = 4 * 4;
 
     for target_len in target_lens {
         for query_len in &query_lens {
@@ -98,8 +98,8 @@ fn run_bench_round(
 
     let mut searcher = Searcher::<Iupac>::new_fwd();
 
-    let mut mini_scanner = mini_searcher::<U32, Scan>::new();
-    let mut mini_searcher = mini_searcher::<U32, Positions>::new();
+    let mut mini_scanner = mini_searcher::<U64, Scan>::new();
+    let mut mini_searcher = mini_searcher::<U64, Positions>::new();
     let encoded = mini_searcher.encode(&queries);
 
     // Benchmark sassy
